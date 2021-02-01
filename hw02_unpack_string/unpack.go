d6e69708e55a314c6655371b492966e7e2250a92
InspectorVitya <49615685+InspectorVitya@users.noreply.github.com>
2021-02-01 14:17:43 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,72 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+	r := []rune(input)
+	if unicode.IsDigit(r[0]) {
+		return "", ErrInvalidString
+	}
+	var b strings.Builder
+	var symbol rune
+	var symbolNext rune
+	strLen := len(r)
+	for i := 0; i < strLen; i++ {
+		symbol = r[i]
+		if strLen <= i+1 {
+			if !unicode.IsDigit(symbol) && symbol != 92 {
+				b.WriteRune(symbol)
+				break
+			} else {
+				break
+			}
+		} else {
+			symbolNext = r[i+1]
+		}
+		if unicode.IsDigit(symbol) && unicode.IsDigit(symbolNext) {
+			return "", ErrInvalidString
+		}
+		switch {
+		case symbol == 92:
+			{
+				if !unicode.IsDigit(symbolNext) && symbolNext != 92 {
+					return "", ErrInvalidString
+				}
+				if strLen > i+2 {
+					if unicode.IsDigit(r[i+2]) {
+						num, _ := strconv.Atoi(string(r[i+2]))
+						b.WriteString(strings.Repeat(string(symbolNext), num))
+						i++
+						continue
+					} else {
+						b.WriteRune(symbolNext)
+						i++
+						continue
+					}
+				} else {
+					b.WriteRune(symbolNext)
+					break
+				}
+			}
+		case unicode.IsDigit(symbolNext):
+			{
+				num, _ := strconv.Atoi(string(symbolNext))
+				b.WriteString(strings.Repeat(string(symbol), num))
+				continue
+			}
+		}
+		if !unicode.IsDigit(symbol) && symbol != 92 {
+			b.WriteRune(symbol)
+		}
+	}
+	return b.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,80 +1,48 @@
 package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-type test struct {
-	input    string
-	expected string
-	err      error
-}
-
 func TestUnpack(t *testing.T) {
-	for _, tst := range [...]test{
-		{
-			input:    "a4bc2d5e",
-			expected: "aaaabccddddde",
-		},
-		{
-			input:    "abccd",
-			expected: "abccd",
-		},
-		{
-			input:    "3abc",
-			expected: "",
-			err:      ErrInvalidString,
-		},
-		{
-			input:    "45",
-			expected: "",
-			err:      ErrInvalidString,
-		},
-		{
-			input:    "aaa10b",
-			expected: "",
-			err:      ErrInvalidString,
-		},
-		{
-			input:    "",
-			expected: "",
-		},
-		{
-			input:    "aaa0b",
-			expected: "aab",
-		},
-	} {
-		result, err := Unpack(tst.input)
-		require.Equal(t, tst.err, err)
-		require.Equal(t, tst.expected, result)
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{input: "abccd", expected: "abccd"},
+		{input: "", expected: ""},
+		{input: "aaa0b", expected: "aab"},
+		{input: "B2lab0", expected: "BBla"},
+		{input: "abd3", expected: "abddd"},
+		{input: "оту0с", expected: "отс"},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\\\44`, expected: `qwe\4444`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
 	}
 }
 
-func TestUnpackWithEscape(t *testing.T) {
-	t.Skip() // NeedRemove if task with asterisk completed
-
-	for _, tst := range [...]test{
-		{
-			input:    `qwe\4\5`,
-			expected: `qwe45`,
-		},
-		{
-			input:    `qwe\45`,
-			expected: `qwe44444`,
-		},
-		{
-			input:    `qwe\\5`,
-			expected: `qwe\\\\\`,
-		},
-		{
-			input:    `qwe\\\3`,
-			expected: `qwe\3`,
-		},
-	} {
-		result, err := Unpack(tst.input)
-		require.Equal(t, tst.err, err)
-		require.Equal(t, tst.expected, result)
+func TestUnpackInvalidString(t *testing.T) {
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qwe\\34`, `qw\ne`}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
 	}
 }
