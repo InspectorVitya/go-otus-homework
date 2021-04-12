bafdea8c98a7c295eb32d7c1286214f5ee5ca213
InspectorVitya <49615685+InspectorVitya@users.noreply.github.com>
2021-04-12 09:17:31 +0300

    finish

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,26 +1,98 @@
 package hw04lrucache
 
+
 type List interface {
-	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
+	Len() int // длина списка
+	Front() *ListItem // первый элемент списка
+	Back() *ListItem // последний элемент списка
+	PushFront(v interface{}) *ListItem  // добавить значение в начало
+	PushBack(v interface{}) *ListItem // добавить значение в конец
+	Remove(i *ListItem) // удалить элемент
+	MoveToFront(i *ListItem) // переместить элемент в начало
 }
 
 type ListItem struct {
-	Value interface{}
-	Next  *ListItem
-	Prev  *ListItem
+	Value interface{}  // значение
+	Next  *ListItem  // следующий элемент
+	Prev  *ListItem // предыдущий элемент
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	length int
+	front  *ListItem
+	back   *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+func (l *list) Len() int  {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  l.front,
+	}
+	return l.pushFront(item)
+}
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Prev:  l.back,
+	}
+	if l.Len() != 0 {
+		l.back.Next = item
+	} else {
+		l.front = item
+	}
+	l.back = item
+	l.length++
+	return l.back
+}
+func (l *list) Remove(item *ListItem) {
+	if item == nil {
+		return
+	}
+	if item.Prev != nil {
+		item.Prev.Next = item.Next
+	}
+	if item.Next != nil {
+		item.Next.Prev = item.Prev
+	}
+	if l.front == item {
+		l.front = item.Next
+	}
+	if l.back == item {
+		l.back = item.Prev
+	}
+	l.length--
+}
+func (l *list) MoveToFront(item *ListItem) {
+	if l.front != item {
+		l.Remove(item)
+		l.pushFront(item)
+	}
+}
+
+
+func (l *list) pushFront(item *ListItem) *ListItem {
+	item.Prev = nil
+	item.Next = l.front
+	if l.Len() != 0 {
+		l.front.Prev = item
+	} else {
+		l.back = item
+	}
+	l.front = item
+	l.length++
+	return l.front
 }
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,61 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var ErrInvalidNumberWorkers = errors.New("invalid number workers")
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if err := checkParamsInput(n, m); err != nil {
+		return err
+	}
+
+	var ch = make(chan Task, len(tasks))
+	wg := sync.WaitGroup{}
+	maxErrs := int32(m)
+	var errCount int32
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			stop := false
+			for val := range ch {
+				if stop {
+					break
+				}
+				if err := val(); err != nil {
+					stop = atomic.AddInt32(&errCount, 1) >= maxErrs
+				} else {
+					stop = atomic.LoadInt32(&errCount) >= maxErrs
+				}
+			}
+		}()
+	}
+	for _, task := range tasks {
+		ch <- task
+	}
+	close(ch)
+	wg.Wait()
+	if errCount >= maxErrs {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+func checkParamsInput(n, m int) error {
+	switch {
+	case n <= 0:
+		return ErrInvalidNumberWorkers
+	case m <= 0:
+		return ErrErrorsLimitExceeded
+	default:
+		return nil
+	}
 }
