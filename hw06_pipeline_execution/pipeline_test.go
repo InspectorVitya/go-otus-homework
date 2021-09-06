5bf87b7641de0ca62158edf2651b8976db592555
InspectorVitya <49615685+InspectorVitya@users.noreply.github.com>
2021-09-06 20:32:30 +0300

    rm file

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -1,4 +1,4 @@
-package hw06
+package hw06pipelineexecution
 
 import (
 	"strconv"
@@ -89,13 +89,5 @@ func TestPipeline(t *testing.T) {
 
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
-	})
-
-	t.Run("in is nil, done is nil", func(t *testing.T) {
-		res := ExecutePipeline(nil, nil, stages...)
-		elem, ok := <-res
-
-		require.False(t, ok)
-		require.Nil(t, elem)
 	})
 }
